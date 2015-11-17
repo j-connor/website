8b33487e47696b0d8ea4c7077e70bae4203f1962
John Connor <hrgplumer@gmail.com>
2015-11-16 22:00:07 -0500

    Add template files in appropriate places; add template html to main layout

--- mywebsite/App_Start/BundleConfig.cs
+++ mywebsite/App_Start/BundleConfig.cs
@@ -9,23 +9,18 @@ namespace mywebsite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/js/jquery-1.11.3.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/js/modernizr.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/js/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/styles.css"));
+                      "~/Content/css/bootstrap.css"));
         }
     }
 }
